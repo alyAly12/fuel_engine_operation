892733b469f13f532633f73e4f96a1910893e6e3
mohamed aly <m.ali@rb-hub.com>
2024-05-15 21:15:58 +0300

    adding the business logic to the Profile UI

--- lib/features/side_menu_feature/presentation/screens/inner_screen/profile_feature/presentation/view/profile_screen.dart
+++ lib/features/side_menu_feature/presentation/screens/inner_screen/profile_feature/presentation/view/profile_screen.dart
@@ -1,7 +1,11 @@
 import 'package:activation_app/core/component/custom_regular_appBar.dart';
 import 'package:activation_app/core/utils/app_strings.dart';
+import 'package:activation_app/features/side_menu_feature/presentation/screens/inner_screen/profile_feature/data/models/profile_info_response_model.dart';
+import 'package:activation_app/features/side_menu_feature/presentation/screens/inner_screen/profile_feature/presentation/profile_bloc/profile_info_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../../../../../core/services/service_locator.dart';
 import '../widgets/profile_screen_body.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -9,14 +13,19 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: PreferredSize(preferredSize: const Size(kBottomNavigationBarHeight, kBottomNavigationBarHeight),
+    final ProfileInfoResponseModel model = ProfileInfoResponseModel();
+    return BlocProvider(
+      create: (context) => ProfileInfoBloc(sl())..add(GetProfileInfoEvent(model)),
+      child: Scaffold(
+        appBar: PreferredSize(preferredSize: const Size(
+            kBottomNavigationBarHeight, kBottomNavigationBarHeight),
           child: CustomRegularAppBar(
-              mainTitle: AppStrings.profile,
-              actionTitle: AppStrings.back,
+            mainTitle: AppStrings.profile,
+            actionTitle: AppStrings.back,
           ),
+        ),
+        body: const ProfileScreenBody(),
       ),
-      body: const ProfileScreenBody(),
     );
   }
 }
--- lib/features/side_menu_feature/presentation/screens/inner_screen/profile_feature/presentation/widgets/profile_screen_body.dart
+++ lib/features/side_menu_feature/presentation/screens/inner_screen/profile_feature/presentation/widgets/profile_screen_body.dart
@@ -1,4 +1,6 @@
+import 'package:activation_app/features/side_menu_feature/presentation/screens/inner_screen/profile_feature/presentation/profile_bloc/profile_info_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import '../../../../../../../../core/component/custom_text_widget.dart';
@@ -6,71 +8,94 @@ import '../../../../../../../../core/utils/app_colors.dart';
 import '../../../../../../../../core/utils/app_strings.dart';
 import '../../../../../../../../core/utils/assets_manager.dart';
 
-
 class ProfileScreenBody extends StatelessWidget {
   const ProfileScreenBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: double.infinity,
-          height: 50,
-          color: Colors.white,
-          child: Padding(
-            padding:  const EdgeInsets.symmetric(horizontal: 30),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SvgPicture.asset(
-                  AssetsManager.person,
-                  height: 24,
-                ),
-                const SizedBox(width: 5,),
-                const CustomTextWidget(
-                  title: 'Ahmed Ali',
-                  color: AppColors.lightTitleColor,
-                  fontSize: 14,
-                  fontWeight: FontWeight.w500,
-                ),
-                const Spacer(),
-                const Icon(Icons.phone,color: AppColors.lightTitleColor,size: 20,),
-                const SizedBox(width: 5,),
-                const CustomTextWidget(
-                  title: '09543213844',
-                  fontSize: 12,
-                  color: AppColors.lightTitleColor,
-                  fontWeight: FontWeight.w400,
-                ),
-              ],
-            ),
-          ),
-        ),
-
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 30,horizontal: 30),
-          child: Row(
+    return BlocBuilder<ProfileInfoBloc, ProfileInfoState>(
+      builder: (context, state) {
+        if(state is ProfileInfoSuccessState){
+          return Column(
             children: [
-              CustomTextWidget(title: AppStrings.status,fontWeight: FontWeight.w500,color: AppColors.lightTitleColor,fontSize: 14,),
-              const Spacer(),
               Container(
-                height: 30,
-                width: 120.h,
-                decoration: BoxDecoration(
-                    color: AppColors.activeColor,
-                    borderRadius: BorderRadius.circular(12)
+                width: double.infinity,
+                height: 50,
+                color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset(
+                        AssetsManager.person,
+                        height: 24,
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                       CustomTextWidget(
+                        title: '${state.profileInfoResponseEntity.data!.user!.name}',
+                        color: AppColors.lightTitleColor,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                      ),
+                      const Spacer(),
+                      const Icon(
+                        Icons.phone,
+                        color: AppColors.lightTitleColor,
+                        size: 20,
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                       CustomTextWidget(
+                        title: '${state.profileInfoResponseEntity.data!.user!.mobile}',
+                        fontSize: 12,
+                        color: AppColors.lightTitleColor,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ],
+                  ),
                 ),
-                child: Center(
-                  child: CustomTextWidget(title: AppStrings.active,color: Colors.white,fontWeight: FontWeight.w600,fontSize: 14,),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 30),
+                child: Row(
+                  children: [
+                    CustomTextWidget(
+                      title: AppStrings.status,
+                      fontWeight: FontWeight.w500,
+                      color: AppColors.lightTitleColor,
+                      fontSize: 14,
+                    ),
+                    const Spacer(),
+                    Container(
+                      height: 30,
+                      width: 120.h,
+                      decoration: BoxDecoration(
+                          color: AppColors.activeColor,
+                          borderRadius: BorderRadius.circular(12)),
+                      child: Center(
+                        child: CustomTextWidget(
+                          title:'${state.profileInfoResponseEntity.data!.user!.status}',
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 14,
+                        ),
+                      ),
+                    )
+                  ],
                 ),
-              )
-
+              ),
             ],
-          ),
-        ),
-      ],
+          );
+        }else{
+          return const Center(child: CircularProgressIndicator(),);
+        }
+
+      },
     );
   }
 }
