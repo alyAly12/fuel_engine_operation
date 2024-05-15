10e541b804ee52310880043820162c25dde7903d
mohamed aly <m.ali@rb-hub.com>
2024-05-15 21:02:49 +0300

    adding bloc & data to service locator

--- lib/core/services/service_locator.dart
+++ lib/core/services/service_locator.dart
@@ -3,6 +3,11 @@ import 'package:activation_app/features/pairing_feature/data/repository/login_re
 import 'package:activation_app/features/pairing_feature/domain/repository/login_repository.dart';
 import 'package:activation_app/features/pairing_feature/domain/use_case/login_use_case.dart';
 import 'package:activation_app/features/pairing_feature/presentation/login_bloc/login_bloc.dart';
+import 'package:activation_app/features/side_menu_feature/presentation/screens/inner_screen/profile_feature/data/data_source/profile_info_data_source.dart';
+import 'package:activation_app/features/side_menu_feature/presentation/screens/inner_screen/profile_feature/data/repository/profile_info_repo_impl.dart';
+import 'package:activation_app/features/side_menu_feature/presentation/screens/inner_screen/profile_feature/domain/repository/profile_info_repository.dart';
+import 'package:activation_app/features/side_menu_feature/presentation/screens/inner_screen/profile_feature/domain/use_case/profile_info_use_case.dart';
+import 'package:activation_app/features/side_menu_feature/presentation/screens/inner_screen/profile_feature/presentation/profile_bloc/profile_info_bloc.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
@@ -33,22 +38,26 @@ void init() {
 void registerRemoteDataSource() {
   sl.registerLazySingleton<SplashRemoteDataSource>(() => SplashRemoteDataSourceImpl(service: sl()));
   sl.registerLazySingleton<LoginDataSource>(() => LoginDataSourceImpl(service: sl()));
+  sl.registerLazySingleton<ProfileInfoDataSource>(() => ProfileInfoDataSourceImpl(service: sl()));
 }
 
 void registerUseCase() {
 
   sl.registerLazySingleton(() => SplashUseCase(sl()));
   sl.registerLazySingleton(() => LoginUseCase(sl()));
+  sl.registerLazySingleton(() => ProfileInfoUseCase(sl()));
 }
 
 void registerRepositories() {
   sl.registerLazySingleton<SplashRepository>(() => SplashRepoImpl(sl()));
   sl.registerLazySingleton<LoginRepository>(() => LoginRepoImpl(loginDataSource: sl()));
+  sl.registerLazySingleton<ProfileInfoRepository>(() => ProfileInfoRepoImpl(sl()));
 }
 
 void registerBloc() {
 
   sl.registerLazySingleton(() => SplashBloc(sl<SplashUseCase>()));
   sl.registerLazySingleton(() => LoginBloc(sl<LoginUseCase>()));
+  sl.registerLazySingleton(() => ProfileInfoBloc(sl<ProfileInfoUseCase>()));
 
 }
